d73bd2ef4fb2c51ebdbbb2cda14993b0dae884e4
JasonChou0219 <jasonchou0219@gmail.com>
2026-01-01 18:12:11 +0800

    通过Controller测试热部署的配置

---
+++ train/src/main/java/com/example/TestController.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, World!";
+    }
+
+    @GetMapping("/hello/{name}")
+    public String hello(@PathVariable String name) {
+        return "Hello, " + name + "!";
+    }
+}
