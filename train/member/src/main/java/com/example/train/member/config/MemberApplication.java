2efc1fcf27ae0b834b769e436e6625f18c3e925a
JasonChou0219 <jasonchou0219@gmail.com>
2026-01-02 19:07:12 +0800

    修改启动日志，添加了logback xml日志配置文件

--- train/member/src/main/java/com/example/train/member/config/MemberApplication.java
+++ train/member/src/main/java/com/example/train/member/config/MemberApplication.java
@@ -3,13 +3,23 @@ package com.example.train.member.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.example.train.member")
 public class MemberApplication {
 
+
+    private static final Logger logger = LoggerFactory.getLogger(MemberApplication.class);
 	public static void main(String[] args) {
-		SpringApplication.run(MemberApplication.class, args);
+        SpringApplication app = new SpringApplication(MemberApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        logger.info("项目启动成功！！");
+        logger.info("Application started at http://localhost:{}", env.getProperty("server.port"));
 	}
+
 
 }
--- train/member/src/main/java/com/example/train/member/controller/TestController.java
+++ train/member/src/main/java/com/example/train/member/controller/TestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 @RestController
 public class TestController {
 
-    @GetMapping("/hello/zihao")
+    @GetMapping("/hello")
     public String hello() {
         return "Hello, World!";
     }
